ac5c17b7c6b474180660c1d8f4584c06d0de8846
Vinicio Ajuchan <viniciojoj@gmail.com>
2025-08-12 12:10:27 -0600

    Revert "Update common.sh"
    This reverts commit af010373e04f7f009cc1e83960b49833e9e56794.

--- bin/common.sh
+++ bin/common.sh
@@ -53,12 +53,12 @@ download_play_official() {
   mkdir -p ${playUnzipDir}
   
   echo "Zip file: $playZipFile"
-  if [ ! -f "play-1.4.5.zip" ]; then
+  if [ ! -f "$playZipFile" ]; then
     echo "Error: Zip file not found."
     exit 1
   fi
   
-  unzip "plan-1.4.5.zip" -d ${playUnzipDir} > /dev/null 2>&1
+  unzip ${playZipFile} -d ${playUnzipDir} > /dev/null 2>&1
 
   PLAY_BUILD_DIR=$(find -name 'framework' -type d | sed 's/framework//')
 
